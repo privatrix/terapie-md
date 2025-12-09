a333afee33381a31b2f7031fcdd749810dc3f74e
Parziva <alexfedco2@gmail.com>
2025-12-09 06:57:09 +0100

    Improve UI accessibility - Fitts Law tweaks

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -27,10 +27,10 @@ function Calendar({
                 nav: "space-x-1 flex items-center",
                 nav_button: cn(
                     buttonVariants({ variant: "outline" }),
-                    "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
+                    "h-9 w-9 bg-transparent p-0 opacity-50 hover:opacity-100"
                 ),
-                nav_button_previous: "absolute left-1",
-                nav_button_next: "absolute right-1",
+                nav_button_previous: "absolute left-2",
+                nav_button_next: "absolute right-2",
                 table: "w-full border-collapse space-y-1",
                 head_row: "flex",
                 head_cell:
@@ -54,8 +54,8 @@ function Calendar({
                 ...classNames,
             }}
             components={{
-                IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-                IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+                IconLeft: ({ ...props }) => <ChevronLeft className="h-6 w-6" />,
+                IconRight: ({ ...props }) => <ChevronRight className="h-6 w-6" />,
             }}
             {...props}
         />

c14e93e7f849755adca85a393a732ba6f6119efd
Parziva <alexanderfedco@gmail.com>
2025-12-18 09:27:27 +0100

    feat: add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter, Outfit } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
@@ -42,6 +43,18 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
+
+        {/* Google Analytics */}
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-34CDSCNPQX" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-34CDSCNPQX');
+          `}
+        </Script>
       </body>
     </html>
   );
